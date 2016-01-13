0081b44cf269fde029ef64bb4420c61e0325be5a
Takuya Tsuchida <omoshetech.t+git@gmail.com>
2016-01-14 06:20:07 +0900

    Refactor the paginate command

--- lib/jekyll/commands/paginate.rb
+++ lib/jekyll/commands/paginate.rb
@@ -1,13 +1,19 @@
 module Jekyll
   module Commands
     class Paginate < Command
-      def self.init_with_program(prog)
-        prog.command(:paginate) do |c|
-          c.syntax 'paginate'
-          c.description 'Generate paginations for categories and tags'
+      class << self
+        def init_with_program(prog)
+          prog.command(:paginate) do |c|
+            c.syntax 'paginate'
+            c.description 'Generate paginations for categories and tags'
 
-          c.action do |args, options|
+            c.action do |_, options|
+              process(options)
+            end
           end
+        end
+
+        def process(options)
         end
       end
     end
